0ec374d93ce7bf0ce7d80e0f4709d85e805d6d29
ZKHulk <zain.khan.7@city.ac.uk>
2024-11-24 20:17:32 +0000

    update HW5

--- HW 5/task2.sql
+++ HW 5/task2.sql
@@ -1,23 +1,23 @@
 --1
 SELECT SUM(bill_total) AS "Income"
-FROM restBill
+FROM restBill;
 
 --2
 SELECT SUM(bill_total) AS "FebIncome"
 FROM restBill
-WHERE bill_date BETWEEN 160201 AND 160229
+WHERE bill_date BETWEEN 160201 AND 160229;
 
 --3
 SELECT AVG(bill_total)
 FROM restBill
-WHERE table_no = 002
+WHERE table_no = 002;
 
 --4
 SELECT MAX(no_of_seats) AS "Max", MIN(no_of_seats) AS "Min", AVG(no_of_seats) AS "Avg"
 FROM restRest_table
-WHERE room_name = "Blue"
+WHERE room_name = 'Blue';
 
 --5
 SELECT COUNT(DISTINCT table_no)
 FROM restBill
-WHERE waiter_no IN (004,002)
+WHERE waiter_no IN (004,002);
